cfb3296ac148f2b332d3e84cb27f2004ef4e8e45
beneugene <beneugene07@example.com>
2020-03-10 17:03:37 +0800

    This is my ptt example

---
+++ PTT八卦版爬蟲.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+#把標題,作者,日期顯示出來,並回傳下一頁連結
+def geturl():
+    headers={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+             "cookie":"over18=1" }
+    res=requests.get(html,headers=headers)
+    soup=BeautifulSoup(res.text,"html.parser")
+    title=soup.select(".title a")
+    author=soup.select(".author")
+    date=soup.select(".date")
+    for i,j,k in zip(title,author,date):
+        print(i.text,j.text,k.text)
+    nextpage=soup.select(".wide:nth-child(3)")
+    for z in nextpage:
+        print(z.get("href"))
+    url=z.get("href")
+    return url
+#選擇想爬取頁數
+page=0
+html="https://www.ptt.cc/bbs/Gossiping/index1.html"
+for i in range(int(input("請輸入想收集幾頁PTT八卦版的資料:"))):
+    html="https://www.ptt.cc"+geturl()
+    page+=1
+    print("第{0}頁".format(page))
